b3ab32590c561c54c2b1e7c012f64172d5f4dc2c
Stephen Kang <jkkang@sfu.ca>
2016-02-01 18:50:53 -0800

    setup.py changes

--- PyCast/setup.py
+++ PyCast/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-	name = 'PyCast',
-	version = '0.1',
-	description = 'Searches and downloads podcasts to the user\'s liking',
-	author = 'Stephen Kang',
-	author_email = 'stephenkang9@gmail.com',
-	url = 'https://github.com/sacert/PyCast',
+    name = 'PyCast',
+    version = '0.1',
+    description = 'Searches and downloads podcasts to the user\'s liking',
+    author = 'Stephen Kang',
+    author_email = 'stephenkang9@gmail.com',
+    url = 'https://github.com/sacert/PyCast',
     packages = [ 'PyCast' ],
     scripts=['bin/PyCast'],
     zip_safe=True
